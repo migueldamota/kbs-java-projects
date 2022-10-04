8b7f6e7b37c3d3d94d6bd7c7c1254d676cfdd56c
Miguel da Mota <miguel@damota.de>
2022-10-04 10:35:22 +0200

    added project `Baumstammvolumen`

---
+++ Baumstammvolumen.java
@@ -0,0 +1,15 @@
+
+class Baumstammvolumen {
+
+    public static void main(String[] args) {
+        
+        int L = 15; // 10;
+        int D = 32; // 30;
+
+        double V = (Math.PI / 4) * Math.pow(D, 2) * L / 10000;
+
+        System.out.println(V);
+        
+    }
+
+}
